c5627fddd4d3cd1f33ce57620cedb5fa14adf72b
diesch111-dotcom <diesch111@gmail.com>
2026-07-07 15:23:12 -0700

    Add files via upload

---
+++ search_codefile2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" search_codefile2.py
+
+A handy snippet to quickly search all the code files in a
+directory/folder for a segment of code. Just put this little 
+ik utility into a folder and run it.  I use it a lot!
+
+using the Spyder IDE on Linux  dns(vegaseat) 7jul2026
+"""
+
+import glob
+import os
+
+# code segment ...
+#search = "with"
+search = ".after"
+
+# use current working directory to look at all python code files
+directory = os.path.join(os.getcwd(), "*.py")
+
+path_list = []
+
+for path in glob.glob(directory):
+    #print(path)  # testing,,,
+    path_list.append(path)
+
+# do the search
+for path in sorted(path_list):
+    dirname, fname = os.path.split(path)
+    linenum = 0
+    with open(path, errors="ignore") as fin:
+        code = fin.read()
+        for line in code.split('\n'):
+            linenum += 1
+            if search in line:
+                print('"{}" in line {} of {}'.format(search, linenum, fname))
