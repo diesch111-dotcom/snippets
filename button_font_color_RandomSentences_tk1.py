177f71b88c341e4b96c69f80c73af563d3c38de3
diesch111-dotcom <diesch111@gmail.com>
2026-07-10 12:08:57 -0700

    Add files via upload

--- button_font_color_RandomSentences_tk1.py
+++ button_font_color_RandomSentences_tk1.py
@@ -1,4 +1,5 @@
 ''' button_font_color_RandomSentences_tk1.py
+
 exploring Tkinter text fonts
 display random sentences from a list using tk.Button()
 button and root expand automatically to fit the text
@@ -9,7 +10,7 @@ lightgreen, moccasin, peachpuff, orange, grey, purple, brown
 also (light=1 to dark=4) hues of colors like
 red1, red2, red3, red4   etc.
 
-tuple examples of common fonts (some might be available just on Windoze)
+tuple examples of common fonts (some might be available just on Windoze?)
 (family, size, weight)
 times48b = ('times', 48, 'bold')
 times20b = ('times', 20, 'bold')
@@ -31,7 +32,7 @@ https://tkdocs.com/shipman/button.html
 https://tkdocs.com/shipman/colors.html
 
 
-works with LinuxMint and Spyder IDE  dns(vegaseat)  15jun2026
+tested using the Spyder IDE on Linux  dns(vegaseat)  9jul2026
 '''
 
 # python3
@@ -49,11 +50,21 @@ sentence_list = [
 'What did the zebra do to earn its stripes?',
 'Is the opposite if a memory a forgettery?',
 'I read the Constitution for the articles.',
+'Door sign at the Microbiology Lab: Staph Only!',
 'I fought the lawn, and the lawn won.',
 'If it is not broken, fix it until it is.',
+'Advice is free: The right answer will cost plenty.',
 'I feel better after I wine a little.',
+'Sex on television cannot hurt you unless you fall off.',
 'South Korea has Seoul!',
+'Am I indecisive? I am not sure!',
+'Procrastination is the art of keeping up with yesterday.',
+'43.3% of statistics are meaningless!',
+'Remember half the people you know are below average.',
+'A waist is a terrible thing to mind.',
+'Energizer bunny arrested, charged with battery.',
 'Beer: It is not just for breakfast anymore.',
+'Smile, it is the second best thing you can do with your lips.',
 'Beer is now cheaper than gas. Drink, do not drive!',
 'Archaeologists will date any old thing.',
 'Vegetarian: Native Indian word for lousy hunter.',
@@ -75,8 +86,8 @@ def setText():
 root = tk.Tk()
 # set the form's title
 root.title('Click on text area')
-x = 20
-y = 10
+x = 25
+y = 20
 # only set upper left corner ULC (x, y) position of root window
 root.geometry(f"+{x}+{y}")
 
