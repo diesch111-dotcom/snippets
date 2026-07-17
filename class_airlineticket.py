50ae74c135386267db8c64327abf1c5366aaa899
diesch111-dotcom <diesch111@gmail.com>
2026-07-17 12:08:13 -0700

    Add files via upload

--- class_airlineticket.py
+++ class_airlineticket.py
@@ -1,18 +1,23 @@
 ''' class_airlineticket.py
 
-A typical class applcation
+A typical class applcation simplfied with a dataclass decorator
 
 
 tested using Linux and Spyder IDE  vegaseat  17jul2026
 '''
 
+# needs Python 3.7+
+from dataclasses import dataclass
+
+
+# hormal class construct
 class AirlineTicket:
     # by convention capitalize class names
     def __init__(self, name, orig, dest, travel_date, travel_class, price):
         # assign parameters to the instance self
         self.name = name
-        self.origination = orig
-        self.destination = dest
+        self.orig = orig
+        self.dest = dest
         self.travel_date = travel_date
         self.travel_class = travel_class
         self.price = price
@@ -20,8 +25,27 @@ class AirlineTicket:
     def info(self):
         # format the info string
         info_str = " {} travels {} class\n from {} to {} on {}"
-        info = info_str.format(self.name, self.travel_class, self.origination,
-                           self.destination, self.travel_date)
+        info = info_str.format(self.name, self.travel_class, self.orig,
+                           self.dest, self.travel_date)
+        return info
+
+
+# the @dataclass decorator simplifies the class construct
+@dataclass
+class AirlineTicket2:
+    # by convention capitalize class names
+    name: str 
+    orig: str 
+    dest: str 
+    travel_date: str 
+    travel_class: str 
+    price: float
+
+    def info(self):
+        # format the info string
+        info_str = " {} travels {} class\n from {} to {} on {}"
+        info = info_str.format(self.name, self.travel_class, self.orig,
+                           self.dest, self.travel_date)
         return info
 
 
@@ -35,7 +59,7 @@ travel_class = "first"
 price = 1820.50
 
 # create unique instance for this traveler
-spitt = AirlineTicket(name, orig, dest, travel_date, travel_class, price)
+spitt = AirlineTicket2(name, orig, dest, travel_date, travel_class, price)
 
 # test this instance
 print(spitt.info())
@@ -43,4 +67,14 @@ print(spitt.info())
 '''
  Stew Pitt travels first class
  from JFK to LAX on 12/23/2026
+'''
+
+print("="*40)
+
+# might be helpfull info
+print(vars(spitt))
+
+'''
+{'name': 'Stew Pitt', 'orig': 'JFK', 'dest': 'LAX', 
+ 'travel_date': '12/23/2026', 'travel_class': 'first', 'price': 1820.5}
 '''
