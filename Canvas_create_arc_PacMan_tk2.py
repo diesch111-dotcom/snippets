a284469d588f9dce35767b381903ac04f82360ec
diesch111-dotcom <diesch111@gmail.com>
2026-07-24 18:52:45 -0700

    Add files via upload

--- Canvas_create_arc_PacMan_tk2.py
+++ Canvas_create_arc_PacMan_tk2.py
@@ -3,7 +3,7 @@
 ''' Canvas_create_arc_PacMan_tk2.py
 
 Explore the Tkinter canvas.create_arc()
-Create a Pac-Man triangle optical illusion using three arcs
+Create a '3 Pac-Man triangle' optical illusion using three arcs
 The arc has a default start at 3 o'cock
 extent is in degrees counterclockwise
 default style is 'pieslice'
@@ -16,8 +16,8 @@ sum of a triangle's inner angles is 180, so each angle is 180/3 = 60
 extent = 360 - 60 = 300
 height = tan(angle_radians) * side/2 = 121
 
-see also
-https://tkdocs.com/shipman/
+see also:
+https://tkdocs.com/shipman/create_arc.html
 
 tested using the Spyder IDE on Linux  vegaseat  19jul2026
 '''
@@ -40,10 +40,10 @@ def getSquare(x, y, radius):
 # root window expands to fit canvas size
 root = tk.Tk()
 # give it a title
-root.title("canvas.create_arc() Pac-Man triangle")
+root.title("3 Pac-Man triangle")
 
 # create a canvas to draw on
-cv = tk.Canvas(root, width=300, height=300, bg='white')
+cv = tk.Canvas(root, width=300, height=340, bg='white')
 cv.pack()
 
 # top
@@ -55,7 +55,5 @@ arc2 = cv.create_arc(getSquare(150-70, 100+121, 30), start=180-120,
 # right bottom
 arc3 = cv.create_arc(getSquare(150+70, 100+121, 30), start=180,
     extent=300, fill='blue', outline='blue')
-
-#line = cv.create_line(50, 130, 250, 180)  # test
 
 root.mainloop()
