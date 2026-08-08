73baf203fd9f4048b637eda2c061d836f0281e80
diesch111-dotcom <diesch111@gmail.com>
2026-08-08 15:41:33 -0700

    Add files via upload

---
+++ urllib_request_image_(tk show).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+''' urllib_request_image_(tk show).py
+
+Retrieve and save an image from a web page
+Show image with tkinter
+
+tested with Spyder IDE on LinuxMint  vegaseat 19jul2026
+'''
+
+import urllib.request
+#import urllib.request as url_open
+
+# find yourself a picture on an internet web page you like
+# (right click on the picture, look under properties and copy the address)
+#picture_page = "http://www.google.com/intl/en/images/logo.gif"
+picture_page = "http://www.smartlinks.org/revenge2.gif"
+
+# open the web page picture and read it into a variable
+my_opener = urllib.request.build_opener()
+my_page = my_opener.open(picture_page)
+my_picture = my_page.read()
+
+# get image file name via slicing
+filename = picture_page[-10:]
+
+print("Image saved as:", filename)
+'''
+Image saved as: venge2.gif
+'''
+
+# save the image file
+with open(filename, "wb") as fout:
+    fout.write(my_picture)
+
+# show image using Tkinter ...
+# PIL ImageTk also allows formats other than gif or png
+from PIL import ImageTk
+import tkinter as tk
+
+root = tk.Tk()
+# only set ULC (x, y) corner position of the root window
+root.geometry("+{}+{}".format(150, 100))
+root.title(filename)
+
+#  to Tkinter PhotoImage object
+tk_image1 = ImageTk.PhotoImage(file=filename)
+
+# display the image on a label (auto expands to size)
+label1 = tk.Label(root,image=tk_image1)
+label1.pack(padx=5, pady=5)
+
+root.mainloop()
