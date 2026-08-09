06ac5ba0f51e1c29dcd50c8b8ca542fe5197c8b0
diesch111-dotcom <diesch111@gmail.com>
2026-08-09 09:37:52 -0700

    Add files via upload

---
+++ zero_topower_zero.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+''' zero_topower_zero.py
+
+By convention 0 to the power of 0 is one,
+Test if Python conforms
+
+tested with Spyder IDE on LinuxMint  vegaseat 19jul2026
+'''
+
+print(0**0)  # 1 (is correct by convention)
