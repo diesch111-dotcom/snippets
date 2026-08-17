46ce4fe5f004eb6440085c1dd3c9caf9059982b0
diesch111-dotcom <diesch111@gmail.com>
2026-08-16 18:41:13 -0700

    Add files via upload

--- listbox_scrollbar_class_tk1.py
+++ listbox_scrollbar_class_tk1.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 ''' listbox_scrollbar_class_tk1.py
 
 A ScrolledListbox class for general application 
-has vertical and horizontal scrollbars
+has vertical and horizontal scrollbars triggered on demand
 
 https://docs.python.org/3/library/tkinter.html
 https://tkdocs.com/shipman/
 
-
-using the Spyder IDE on Linux  dns aka vegaseat 4jul2026
+tested with IDLE IDE on LinuxMint  VegasEat 19jul2026
 '''
 
 import tkinter as tk
@@ -18,11 +19,9 @@ class ScrolledListbox(tk.Frame):
         tk.Frame.__init__(self,  *args, **kwds)
         self._create_gui(**kwds)
 
-    def _create_gui(self, width=35, height=20, **kwds):
-        "give width and height some default values"
-        print(width, height)  # test
+    def _create_gui(self, width, height, **kwds):
         self.listbox = tk.Listbox(self, width=width, height=height, **kwds)
-        self.listbox.grid(row=1, column=1, sticky="nsew")
+        self.listbox.grid(row=1, column=1, sticky="nsew", padx=5)
         self.scrollx = tk.Scrollbar(self, orient="horizontal", 
                                     command=self.listbox.xview)
         self.scrollx.grid(row=2, column=1, sticky="ew")
@@ -44,22 +43,32 @@ class ScrolledListbox(tk.Frame):
 
 
 # create a list of items to put into the listbox
-# long enough to activate the vertical and horizontal scrolls
-# the laat item is some sort of a ruler
-itemslist = [
-'Stew', 'Tom', 'Jen', 'Adam', 'Ethel', 'Barb', 'Tiny', 'Asta',
-'and now an extended string to activate the horizontal scrolling',
-'Tim', 'Pete', 'Sue', 'Egon', 'Fuzzy', 'Swen', 'Albert', 'Zoey',
-'123456789_123456789_123456789_123456789__123456789_']
+# long and wide enough to trigger the vertical and horizontal scrolls
+arduino_sensor_list = ['DHT11 temperature and humidity sensor module',
+ 'HC-SR501 infrared human body induction module',
+ 'DS1302 real time clock module',
+ 'The rain sensor module for outdoor use',
+ 'Sound sensor module with microphone',
+ 'HC-SR04 ultrasonic sensor',
+ 'The industrial grade flame sensor module',
+ 'KY-008 laser head sensor/emitter module',
+ 'CdS Photo sensitive resistance sensor module',
+ 'The YL-69 soil moisture sensor',
+ 'IR obstacle avoidance sensor',
+ 'Vibration sensor module',
+ 'MQ-2 gas sensor module',
+ '315M super regenerative receiver module',
+ 'The tilt sensor module',
+ 'The all time famous robot special low price path tracing module']
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Scrolled Listbox")
-    frame = ScrolledListbox(root, width=40, height=12)
+    root.title("Arduino sensor sale...")
+    frame = ScrolledListbox(root, width=40, height=12, bg="wheat")
     frame.grid()
     # load the listbox
-    for item in itemslist:
+    for item in arduino_sensor_list:
         frame.listbox.insert("end", item)
 
     root.mainloop()
