69aa55c57b41b3650c7021e5bb3a3457fe0a092c
diesch111-dotcom <diesch111@gmail.com>
2026-08-31 21:37:48 -0700

    Add files via upload

--- pqt6_Painter_drawRect.py
+++ pqt6_Painter_drawRect.py
@@ -8,17 +8,13 @@ there are a number of ways colors can be specified
 most flexible ...
 QColor(r, g, b, alpha=255)
 where r, g, b are red, green, blue values as integers 0 to 255
-alpha is the transparency value (0 --> most transparent)
+alpha is the transparency value 0 to 255 (0 is most transparent)
 
-QColor(0, 0, 0) --> black
-or use "#rrggbb" string
-QColor("#ff0000") --> red
-
-148 color names are available ...
+color the most descriptive way, 148 color names are available ...
 QColor('black')
 
 fill colors are set with the brush
-perimeter colors are set with the pen
+perimeter (outline) colors are set with the pen
 
 featuring...
 qp = QPainter()
@@ -28,23 +24,22 @@ qp.setBrush(QColor("color-name"))
 qp.drawRect(int x, int y, int width, int height)
 qp.end()
 
-tested with VSCodium IDE on LinuxMint  VegasEat 28aug2026
+tested with VSCodium IDE on LinuxMint  VegasEat 31aug2026
 '''
 
-# for QPainter, QColor, 
-from PyQt6.QtGui import *
-# for bread and butter items QWidget, QPushButton, QLabel etc
-from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication
 
 
-class MyWindow(QWidget):
+class Canvas(QWidget):
 
     def __init__(self):
         QWidget.__init__(self)
+        # QWidget or 'self' is the canvas to draw on...
         # setGeometry(x_pos, y_pos, width, height)
         # upper left corner coordinates (x_pos, y_pos)
         self.setGeometry(300, 300, 370, 100)
-        self.setWindowTitle('Colors set with brush and pen')
+        self.setWindowTitle('One overlapping rectangle has transparency')
 
     def paintEvent(self, e):
         '''
@@ -64,31 +59,33 @@ class MyWindow(QWidget):
         # QPen(color, width, style)
         qp.setPen(QColor('black'))
 
-        # can use QColor(r, g, b) with values 0 to 255
-        # qp.setBrush(QColor(255, 0, 0)) or
-        qp.setBrush(QColor("#ff0000"))
+        qp.setBrush(QColor("red"))
         # drawRect(int x, int y, int width, int height)
         # upper left corner coordinates (x, y)
         qp.drawRect(10, 15, 90, 60)
 
-        # use a preset named color
         qp.setBrush(QColor('green'))
-        qp.drawRect(160, 25, 90, 60)
-
-        # this rectangle will overlap the previous one
-        # you can give it some transparency alpha 0 to 255
-        # QColor(int r, int g, int b, int alpha=255)
-        qp.setBrush(QColor(0, 0, 255, 100))
         qp.drawRect(130, 15, 90, 60)
 
-        # some colors can be given as preset color strings
+        # this rectangle will overlap the previous one
+        # so give it some transparency alpha 0 to 255
+        # 0 is most transparent, 255 is not transparent (default)
+        # QColor(int r, int g, int b, int alpha=255)
+        r = 0
+        g = 0
+        b = 255 
+        alpha = 130
+        qp.setBrush(QColor(r, g, b, alpha))
+        qp.drawRect(160, 25, 90, 60)
+
         qp.setBrush(QColor('yellow'))
-        qp.drawRect(265, 25, 90, 60)
+        qp.drawRect(265, 15, 90, 60)
 
 
 app = QApplication([])
-# forms the drawing canvas
-wind = MyWindow()
-wind.show()
+# form the drawing canvas
+cv = Canvas()
+cv.show()
 # run the application event loop
 app.exec()
+
